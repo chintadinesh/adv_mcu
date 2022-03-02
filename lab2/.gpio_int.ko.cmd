537a12a3d605d2ff07e8260891a8a4134aad1bda
chintadinesh <cdineshrdy@gmail.com>
2022-03-01 20:44:49 -0600

    lab1 submission remade

---
+++ lab2/.gpio_int.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/code/ULTRA/gpio-int-latency_jiffies/gpio_int.ko := ld -EL -r  -maarch64linux -T ./scripts/module-common.lds --build-id  -o /root/code/ULTRA/gpio-int-latency_jiffies/gpio_int.ko /root/code/ULTRA/gpio-int-latency_jiffies/gpio_int.o /root/code/ULTRA/gpio-int-latency_jiffies/gpio_int.mod.o ;  true
---
+++ lab2/.zed_gpio_int.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/code/ULTRA/gpio-int-latency/zed_gpio_int.ko := ld -EL -r  -maarch64linux -T ./scripts/module-common.lds --build-id  -o /root/code/ULTRA/gpio-int-latency/zed_gpio_int.ko /root/code/ULTRA/gpio-int-latency/zed_gpio_int.o /root/code/ULTRA/gpio-int-latency/zed_gpio_int.mod.o ;  true
