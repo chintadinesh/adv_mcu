3c48f90944aba1a1aa5cb09671a31f09285b3bf9
Dinesh Reddy Chinta <cdineshrdy@gmail.com>
2022-03-02 13:11:24 -0600

    capture timer debug registers order corrected

---
+++ lab2/kernel_module/setup_cdma.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm /dev/cdma_int
+/bin/mknod /dev/cdma_int c 240 0
+
+/sbin/rmmod cdma_int
+/sbin/insmod cdma_int.ko
+
+
+#/bin/pm 0x43c00000 0x55 > /dev/null
+#sleep 1.0
+#/bin/pm 0x43c00000 0xaa > /dev/null
+#sleep 1.0
+#cat /proc/interrupts | grep cdma
+
+#while (ls > /dev/null) do ./intr_latency.exe; cat /proc/interrupts | grep cdma; done
+
